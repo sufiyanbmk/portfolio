c00f79e212cf0b590f92ed29975627ccd9746a6e
sufiyanbmk <sufiyanbmk@gmail.com>
2024-06-21 21:35:10 +0400

    Minor:Fixed alignment issue in  Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className="absolute bottom-10 md:bottom-32 w-full flex justify-center items-center md:flex hidden">
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#work">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,7 +22,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 1.2}
-        position={isMobile ? [0, -3, -2.2] : [0, -1.75, -6.5]}
+        position={isMobile ? [0, -1, 0] : [0, -1.75, -6.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
